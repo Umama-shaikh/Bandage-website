6c44756e4984d0ea5bced33ac031415a03faf906
Umama-shaikh <160320031+Umama-shaikh@users.noreply.github.com>
2024-12-20 01:17:38 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import HeroSection from "@/components/hero"
+import FeaturedPosts from "@/components/feature-post"
+import CoupleDiv from "@/components/couplediv"
+import GreenDiv from "@/components/greendiv"
+import BestProducts from "@/components/best-products"
+import EditorsPick from "@/components/editors"
+export default function Home() {
+  return (
+    <>
+    <HeroSection/>
+    <EditorsPick/>
+    <BestProducts/>
+    <GreenDiv/>
+    <CoupleDiv/>
+    <FeaturedPosts/>
+    
+    </>
+    
+  )
+}
